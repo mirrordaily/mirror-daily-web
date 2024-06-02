b3d6e075961d12b5b6c2538ae57210b149d0c227
Tsuki Akiba <erase2004@gmail.com>
2024-06-02 19:03:24 +0800

    feat: add config

---
+++ constants/config.ts
@@ -0,0 +1,26 @@
+import { ENVIRONMENT } from './misc'
+
+const ENV = (function () {
+  const env = process.env.NEXT_PUBLIC_ENV
+
+  if (!env) return ENVIRONMENT.LOCAL
+  else if (Object.values(ENVIRONMENT).includes(env as ENVIRONMENT)) {
+    return env as ENVIRONMENT
+  }
+  return ENVIRONMENT.LOCAL
+})()
+
+let API_ENDPOINT = ''
+
+switch (ENV) {
+  case ENVIRONMENT.LOCAL:
+  case ENVIRONMENT.DEVELOPMENT:
+    // TODO: switch to gql API endpoint service
+    API_ENDPOINT = 'https://daily-cms-dev-axzdcnzvtq-de.a.run.app/api/graphql'
+    break
+
+  default:
+    break
+}
+
+export { API_ENDPOINT, ENV }
---
+++ constants/misc.ts
@@ -0,0 +1,6 @@
+export enum ENVIRONMENT {
+  LOCAL = 'local',
+  DEVELOPMENT = 'dev',
+  STAGING = 'staging',
+  PRODUCTION = 'prod',
+}
