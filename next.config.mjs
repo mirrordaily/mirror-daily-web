5ec681402b0738997c5b622453fa6a17e08b36e5
nihclil <oxx921@gmail.com>
2024-06-11 11:14:08 +0800

    chore: remove setting from next.config

--- next.config.mjs
+++ next.config.mjs
@@ -3,15 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'standalone',
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'v3-statics-dev.mirrormedia.mg',
-        pathname: '**',
-      },
-    ],
-  },
 }
 
 export default nextConfig
