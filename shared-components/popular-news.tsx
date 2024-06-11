87ac00a58feeffc802beffcacdd9493cb5ddb20d
nihclil <oxx921@gmail.com>
2024-06-11 11:24:05 +0800

    refactor: update PopularNews styles

--- shared-components/popular-news.tsx
+++ shared-components/popular-news.tsx
@@ -4,10 +4,10 @@ import type { ReactElement } from 'react'
 
 export default function PopularNews(): ReactElement {
   return (
-    <section className="md: hidden md:flex md:w-[588px] md:flex-col md:items-center md:gap-y-[31px] lg:w-[240px] lg:gap-y-[19px]">
-      <h3 className="text-lg font-bold leading-normal text-mirror-500">
+    <section className="hidden md:flex md:w-[588px] md:flex-col md:items-center md:gap-y-[31px] lg:w-[240px] lg:gap-y-[19px]">
+      <div className="text-lg font-bold leading-normal text-mirror-500">
         熱門新聞
-      </h3>
+      </div>
       <div className="grid md:grid-cols-2 md:gap-7 lg:grid-cols-1 lg:gap-y-5">
         {popularNews.map((news) => (
           <UiPopularNewsCard key={news.id} news={news} />
