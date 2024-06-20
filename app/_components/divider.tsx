f6d77d275821ffd944f3440ee015a3cbdd768d44
Tsuki Akiba <erase2004@gmail.com>
2024-06-20 16:30:26 +0800

    refactor: update section-in-homepage style

--- app/_components/divider.tsx
+++ app/_components/divider.tsx
@@ -1,6 +1,6 @@
 export default function SectionDivider() {
   return (
-    <div className="section-in-homepage w-full md:max-w-screen-md lg:max-w-none">
+    <div className="section-in-homepage">
       <hr className="h-px border-none bg-[#000928]" />
     </div>
   )
