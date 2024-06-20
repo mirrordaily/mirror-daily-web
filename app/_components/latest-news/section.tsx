14f14d2eda88b0aa8c8c5395ac6fa67ce8b3024b
Tsuki Akiba <erase2004@gmail.com>
2024-06-20 11:46:39 +0800

    feat(index): add LatestNewsSection

---
+++ app/_components/latest-news/section.tsx
@@ -0,0 +1,10 @@
+export default async function LatestNewsSection() {
+  return (
+    <section className="section-in-homepage w-full md:max-w-screen-md lg:max-w-none">
+      <p className="mb-[34px] text-base font-bold leading-normal text-[#119CC7] md:mb-[26px] md:text-lg">
+        最新新聞
+      </p>
+      <div className="flex w-full flex-col items-center justify-center gap-y-4 pb-8 md:flex-row md:flex-wrap md:gap-x-10 md:gap-y-7 md:pb-[27px] lg:gap-x-8 lg:pb-12 [&>*:last-child]:flex [&>*:last-child]:w-full [&>*:last-child]:justify-center"></div>
+    </section>
+  )
+}
