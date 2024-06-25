643730af9567f5d01a736babb558d8b4ef2fbc46
Tsuki Akiba <erase2004@gmail.com>
2024-06-25 15:44:58 +0800

    refactor(index): update data handle in TopNewsSection

--- app/_components/top-news/section.tsx
+++ app/_components/top-news/section.tsx
@@ -1,14 +1,57 @@
 /* 即時新聞、熱門新聞 */
 import TopNewsMain from './main'
-import { fetchLatestPost, fetchPopularPost } from '@/app/action'
+import { fetchLatestPost, fetchPopularPost, fetchLiveEvent } from '@/app/action'
+import type { ParameterOfComponent } from '@/types/common'
+import { getPostPageUrl } from '@/utils/site-urls'
 
 export default async function TopNewsSection() {
+  const liveEvent = await fetchLiveEvent()
   const latestPosts = await fetchLatestPost(0)
   const popularPosts = await fetchPopularPost()
 
-  const postsOfTab: Parameters<typeof TopNewsMain>[0]['postsOfTab'] = {
-    Latest: latestPosts.slice(0, 10),
-    Hot: popularPosts,
+  let latestList: ParameterOfComponent<
+    typeof TopNewsMain
+  >['postsOfTab']['Latest'] = [undefined]
+
+  if (liveEvent) {
+    latestList = [liveEvent, ...latestPosts.slice(0, 9)]
+  } else {
+    const first = latestPosts[0]
+
+    if (first) {
+      latestList = [
+        {
+          postName: first.postName,
+          heroImage: first.heroImage,
+          link: getPostPageUrl(first.postSlug),
+        },
+        ...latestPosts.slice(1, 9),
+      ]
+    }
+  }
+
+  let hotList: ParameterOfComponent<typeof TopNewsMain>['postsOfTab']['Hot'] = [
+    undefined,
+  ]
+
+  {
+    const first = popularPosts[0]
+
+    if (first) {
+      hotList = [
+        {
+          postName: first.postName,
+          heroImage: first.heroImage,
+          link: getPostPageUrl(first.postSlug),
+        },
+        ...popularPosts.slice(1, 9),
+      ]
+    }
+  }
+
+  const postsOfTab: ParameterOfComponent<typeof TopNewsMain>['postsOfTab'] = {
+    Latest: latestList,
+    Hot: hotList,
   }
 
   return (
