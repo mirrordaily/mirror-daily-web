d08f671add79a4317f750ff9eaf0d0b360225320
Tsuki Akiba <erase2004@gmail.com>
2024-06-25 15:04:23 +0800

    feat: add getTraceObject util

--- utils/log/common.ts
+++ utils/log/common.ts
@@ -1,19 +1,21 @@
 import { ApolloError } from '@apollo/client'
 import errors from '@twreporter/errors'
+import { GCP_PROJECT_ID } from '@/constants/config'
+import type { headers } from 'next/headers'
 
-const logGQLError = (
+const createErrorLogger = (
   errorMessage: string,
   traceObject?: Record<string, unknown> | undefined
 ) => {
-  return (gqlErrors: unknown) => {
+  return (error: unknown) => {
     const annotatingError = errors.helpers.wrap(
-      gqlErrors,
+      error,
       'UnhandledError',
       errorMessage
     )
 
-    if (gqlErrors instanceof ApolloError) {
-      const { graphQLErrors, clientErrors, networkError } = gqlErrors
+    if (error instanceof ApolloError) {
+      const { graphQLErrors, clientErrors, networkError } = error
       console.error(
         JSON.stringify({
           severity: 'ERROR',
@@ -34,6 +36,29 @@ const logGQLError = (
           ...(traceObject ?? {}),
         })
       )
+    } else if (error instanceof Error) {
+      console.error(
+        JSON.stringify({
+          severity: 'ERROR',
+          message: errors.helpers.printAll(
+            annotatingError,
+            {
+              withStack: true,
+              withPayload: true,
+            },
+            0,
+            0
+          ),
+          debugPayload: {
+            error: {
+              name: error.name,
+              message: error.message,
+              stack: error.stack,
+            },
+          },
+          ...(traceObject ?? {}),
+        })
+      )
     } else {
       console.error(
         JSON.stringify({
@@ -48,7 +73,7 @@ const logGQLError = (
             0
           ),
           debugPayload: {
-            gqlErrors,
+            error,
           },
           ...(traceObject ?? {}),
         })
@@ -57,4 +82,15 @@ const logGQLError = (
   }
 }
 
-export { logGQLError }
+const getTraceObject = (header: ReturnType<typeof headers>) => {
+  const traceHeader = header.get('x-cloud-trace-context')
+  const globalLogFields: Record<string, string> = {}
+  if (traceHeader) {
+    const [trace] = traceHeader.split('/')
+    globalLogFields['logging.googleapis.com/trace'] =
+      `projects/${GCP_PROJECT_ID}/traces/${trace}`
+  }
+  return globalLogFields
+}
+
+export { createErrorLogger, getTraceObject }
