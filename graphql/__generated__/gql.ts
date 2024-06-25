2d1c5fd00f3443083e7a4f5f972f5765d310c0c2
nihclil <oxx921@gmail.com>
2024-06-25 14:04:48 +0800

    chore: graphql codegen

--- graphql/__generated__/gql.ts
+++ graphql/__generated__/gql.ts
@@ -2,8 +2,20 @@
 import * as types from './graphql'
 import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core'
 
-
-const documents = []
+/**
+ * Map of all GraphQL operations in the project.
+ *
+ * This map has several performance disadvantages:
+ * 1. It is not tree-shakeable, so it will include all operations in the project.
+ * 2. It is not minifiable, so the string of a GraphQL query will be multiple times inside the bundle.
+ * 3. It does not support dead code elimination, so it will add unused operations.
+ *
+ * Therefore it is highly recommended to use the babel or swc plugin for production.
+ */
+const documents = {
+  'query GetPostsBySectionSlug($skip: Int!, $take: Int, $where: PostWhereInput!) {\n  posts(skip: $skip, take: $take, where: $where, orderBy: {publishedDate: desc}) {\n    title\n    createdAt\n    brief\n    heroImage {\n      id\n      resized {\n        w1200\n        w1600\n        w2400\n        w480\n        w800\n        original\n      }\n      resizedWebp {\n        original\n        w1200\n        w1600\n        w2400\n        w480\n        w800\n      }\n    }\n    slug\n  }\n}':
+    types.GetPostsBySectionSlugDocument,
+}
 
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
@@ -18,6 +30,13 @@ const documents = []
  * Please regenerate the types.
  */
 export function gql(source: string): unknown
+
+/**
+ * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function gql(
+  source: 'query GetPostsBySectionSlug($skip: Int!, $take: Int, $where: PostWhereInput!) {\n  posts(skip: $skip, take: $take, where: $where, orderBy: {publishedDate: desc}) {\n    title\n    createdAt\n    brief\n    heroImage {\n      id\n      resized {\n        w1200\n        w1600\n        w2400\n        w480\n        w800\n        original\n      }\n      resizedWebp {\n        original\n        w1200\n        w1600\n        w2400\n        w480\n        w800\n      }\n    }\n    slug\n  }\n}'
+): (typeof documents)['query GetPostsBySectionSlug($skip: Int!, $take: Int, $where: PostWhereInput!) {\n  posts(skip: $skip, take: $take, where: $where, orderBy: {publishedDate: desc}) {\n    title\n    createdAt\n    brief\n    heroImage {\n      id\n      resized {\n        w1200\n        w1600\n        w2400\n        w480\n        w800\n        original\n      }\n      resizedWebp {\n        original\n        w1200\n        w1600\n        w2400\n        w480\n        w800\n      }\n    }\n    slug\n  }\n}']
 
 export function gql(source: string) {
   return (documents as any)[source] ?? {}
