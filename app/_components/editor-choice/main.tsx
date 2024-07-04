ccb1486d14946895c6bfc0480397a141b1d603ea
Tsuki Akiba <erase2004@gmail.com>
2024-07-04 21:50:24 +0800

    feat(index): add TabButton component

--- app/_components/editor-choice/main.tsx
+++ app/_components/editor-choice/main.tsx
@@ -1,9 +1,29 @@
 'use client'
+
+import TabButton from './tab-button'
+import { useState } from 'react'
+
 export default function EditorChoiceMain() {
+  const TAB = {
+    EDITOR_CHOICE: '編輯精選',
+    AI_CHOICE: 'AI 精選',
+  } as const
+
+  type ValueOfTab = (typeof TAB)[keyof typeof TAB]
+
+  const [activeTab, setActiveTab] = useState<ValueOfTab>(TAB.EDITOR_CHOICE)
+
   return (
     <div className="relative w-full">
       <div className="z-over-editor-choice mb-4 flex justify-center gap-x-2 md:absolute md:left-4 md:top-4 lg:left-7">
-        {/* Tab Buttons */}
+        {Object.values(TAB).map((value) => (
+          <TabButton
+            key={value}
+            text={value}
+            isActive={activeTab === value}
+            onClick={() => setActiveTab(value)}
+          />
+        ))}
       </div>
       {/* Swiper Component */}
     </div>
