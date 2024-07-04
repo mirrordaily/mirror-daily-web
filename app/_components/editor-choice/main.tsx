e7ac726c2ad285f1f484a00c1a6c656df19e0b3d
Tsuki Akiba <erase2004@gmail.com>
2024-07-04 21:47:52 +0800

    feat(index): add EditorChoiceSection

---
+++ app/_components/editor-choice/main.tsx
@@ -0,0 +1,11 @@
+'use client'
+export default function EditorChoiceMain() {
+  return (
+    <div className="relative w-full">
+      <div className="z-over-editor-choice mb-4 flex justify-center gap-x-2 md:absolute md:left-4 md:top-4 lg:left-7">
+        {/* Tab Buttons */}
+      </div>
+      {/* Swiper Component */}
+    </div>
+  )
+}
---
+++ app/_components/editor-choice/section.tsx
@@ -0,0 +1,9 @@
+import EditorChoiceMain from './main'
+
+export default function EditorChoiceSection() {
+  return (
+    <section className="section-in-homepage mb-9 mt-4 px-0 md:my-5 md:px-5 lg:mb-9 lg:mt-7 lg:px-9">
+      <EditorChoiceMain />
+    </section>
+  )
+}
