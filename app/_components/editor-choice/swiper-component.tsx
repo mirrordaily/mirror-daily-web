a481d35827c67ccd03b964afbbeb04ae20551796
Tsuki Akiba <erase2004@gmail.com>
2024-07-09 00:50:26 +0800

    feat(index): add image mask

--- app/_components/editor-choice/swiper-component.tsx
+++ app/_components/editor-choice/swiper-component.tsx
@@ -46,11 +46,12 @@ export default function SwiperComponent({ list }: Props) {
               target="_blank"
               className="group/slide w-full"
             >
-              <div className="aspect-[375/208] w-full overflow-hidden md:aspect-[680/379] lg:aspect-[1127/628]">
+              <div className="relative aspect-[375/208] w-full overflow-hidden md:aspect-[680/379] lg:aspect-[1127/628]">
                 <CustomImage
                   images={heroImage.resized}
                   className="w-full group-hover/slide:scale-110 md:aspect-[680/379] md:group-hover/slide:scale-100 md:group-active/slide:scale-100 lg:aspect-[1127/628]"
                 />
+                <div className="absolute inset-0 md:bg-[linear-gradient(180deg,rgba(0,0,0,0.48)_6.69%,rgba(110,110,110,0.24)_17.20%,rgba(217,217,217,0.00)_20.52%,rgba(255,255,255,0)_23.25%,rgba(255,255,255,0)_74.52%,rgba(217,217,217,0.00)_74.52%,rgba(43,43,43,0.64)_83.44%,rgba(0,0,0,0.80)_96.18%)]" />
               </div>
               <p className="mx-6 mt-4 line-clamp-3 h-[84px] text-xl font-bold leading-normal text-[#000928] group-hover/slide:text-[#575D71] group-active/slide:text-[#575D71] md:absolute md:bottom-3 md:left-5 md:m-0 md:line-clamp-2 md:h-auto md:max-h-[58px] md:w-[440px] md:text-white md:group-hover/slide:text-white md:group-hover/slide:underline md:group-active/slide:text-white md:group-active/slide:underline lg:bottom-7 lg:left-7 lg:max-h-[69px] lg:w-[654px] lg:text-2xl lg:font-black">
                 {postName}
