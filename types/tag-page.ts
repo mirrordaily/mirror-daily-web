64090b2e4f6b76696ae648eb3e585266ae53397b
nihclil <oxx921@gmail.com>
2024-07-22 16:54:15 +0800

    feat: add tag-page types

---
+++ types/tag-page.ts
@@ -0,0 +1,15 @@
+import type { HeroImage } from './common'
+
+export type TagPost = {
+  title: string
+  createdTime: string
+  brief: string
+  heroImage: HeroImage
+  link: string
+  sectionColor: string
+  sectionName: string
+}
+
+export type TagInfo = {
+  name: string
+}
