e9c42d5518dd351bd6cbc15b6d6d3be5eeb1d353
nihclil <oxx921@gmail.com>
2024-08-02 21:56:23 +0800

    refactor: remove debug message and add TODO comments

--- app/error.tsx
+++ app/error.tsx
@@ -7,7 +7,7 @@ import { useEffect } from 'react'
 
 export default function Error({ error }: { error: Error }) {
   useEffect(() => {
-    console.error(error)
+    // TODO: send error log to GCP Logging
   }, [error])
 
   return (
--- app/global-error.tsx
+++ app/global-error.tsx
@@ -7,7 +7,7 @@ import { useEffect } from 'react'
 
 export default function GlobalError({ error }: { error: Error }) {
   useEffect(() => {
-    console.error(error)
+    // TODO: send error log to GCP Logging
   }, [error])
 
   return (
