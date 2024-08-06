5bfaacc59ddd84a28ae40cca23b2b2dda77dc786
nihclil <oxx921@gmail.com>
2024-08-06 17:04:50 +0800

    styles: remove flex

--- shared-components/error/ui-500.tsx
+++ shared-components/error/ui-500.tsx
@@ -19,7 +19,7 @@ export default function Custom500() {
   }, [])
 
   return (
-    <main className="mb-[72px] flex flex-col items-center">
+    <main className="mb-[72px]">
       <hr className="mt-4 hidden w-[1128px] border border-[#000928] lg:block" />
       <div className="mt-20 flex flex-col items-center gap-y-[72px] md:mt-[92px] md:gap-y-[88px] lg:mt-[104px]">
         <ErrorMessage
