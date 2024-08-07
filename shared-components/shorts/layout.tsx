d337d53043606e4b47a11e17e1b580436f41eb2f
Tsuki Akiba <erase2004@gmail.com>
2024-08-07 22:35:55 +0800

    feat(shorts): add shared layout

---
+++ shared-components/shorts/layout.tsx
@@ -0,0 +1,12 @@
+export default function ShortsLayout() {
+  return (
+    <div className="relative flex h-screen max-h-screen w-full max-w-screen-sm flex-col md:max-w-screen-lg">
+      {/* Header */}
+      <div className="flex grow flex-col overflow-hidden md:flex-row md:px-5 md:pt-[var(--shorts-body-padding)]">
+        {/* Navbar */}
+        {/* Body */}
+      </div>
+      {/* Mobile Navbar */}
+    </div>
+  )
+}
