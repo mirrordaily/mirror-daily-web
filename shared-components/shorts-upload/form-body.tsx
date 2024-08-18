27c6e988acabb39f7ca98e804969a38f84f708ee
Tsuki Akiba <erase2004@gmail.com>
2024-08-19 01:01:23 +0800

    feat: add InitialForm to FormBody

--- shared-components/shorts-upload/form-body.tsx
+++ shared-components/shorts-upload/form-body.tsx
@@ -1,4 +1,23 @@
 'use client'
+
+import { useAppDispatch, useAppSelector } from '@/redux/hooks'
+import { selectProgress } from '@/redux/shorts-upload/selector'
+import { FormProgress } from '@/types/shorts'
+import InitialForm from './initial-form'
+
 export default function FormBody() {
-  return <div className="flex w-full grow flex-col"></div>
+  /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
+  const dispatch = useAppDispatch()
+  const progress = useAppSelector(selectProgress)
+  return (
+    <div className="flex w-full grow flex-col">
+      <div
+        className={`${
+          progress === FormProgress.Initial ? 'flex' : 'hidden'
+        } my-auto flex-col gap-y-6 self-center text-center`}
+      >
+        <InitialForm />
+      </div>
+    </div>
+  )
 }
---
+++ shared-components/shorts-upload/initial-form.tsx
@@ -0,0 +1,40 @@
+import type { ChangeEvent } from 'react'
+import { useRef } from 'react'
+import { AVAILABLE_VIDEO_MIME_TYPE } from '@/constants/multimedia'
+import { useAppDispatch, useAppSelector } from '@/redux/hooks'
+import { changeShortsFile } from '@/redux/shorts-upload/slice'
+import { selectShorts } from '@/redux/shorts-upload/selector'
+import BackButton from './back-button'
+import CustomText from './custom-text'
+
+export default function InitialForm() {
+  const dispatch = useAppDispatch()
+  const { hasError } = useAppSelector(selectShorts)
+  const inputRef = useRef<HTMLInputElement>(null)
+  const inputChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files
+    const file = files instanceof FileList ? files[0] : files
+
+    dispatch(changeShortsFile(file))
+  }
+
+  return (
+    <>
+      <input
+        className="hidden"
+        name="shorts"
+        type="file"
+        accept={AVAILABLE_VIDEO_MIME_TYPE.join(',')}
+        ref={inputRef}
+        onChange={inputChangeHandler}
+      />
+      <BackButton type="button" clickFn={() => inputRef.current?.click()}>
+        選取檔案
+      </BackButton>
+      <CustomText
+        content="上傳失敗！"
+        colorClass={hasError ? 'text-[#D94141]' : 'text-transparent'}
+      />
+    </>
+  )
+}
