60ba1a59c225d39231ff22684484e2a98e6832e9
Tsuki Akiba <erase2004@gmail.com>
2024-08-19 00:52:45 +0800

    refactor: add ModalBody to UploadModal

---
+++ shared-components/shorts-upload/modal-body.tsx
@@ -0,0 +1,58 @@
+'use client'
+
+import { useFormState } from 'react-dom'
+import { createCreativityShorts } from '@/app/actions-general'
+import { FormState } from '@/types/shorts'
+import { getTailwindConfig } from '@/utils/tailwind'
+import { useWindowSize } from 'usehooks-ts'
+import MobileFormBody from './mobile-form-body'
+import NextButton from './next-button'
+
+type Props = {
+  closeHandler: () => void
+}
+
+export default function ModalBody({ closeHandler }: Props) {
+  const config = getTailwindConfig()
+  const desktopLowerBound = Number(config.theme.screens.lg.split('px')[0])
+  const { width } = useWindowSize()
+  const isDesktop = width >= desktopLowerBound
+  const [response, formAction] = useFormState(createCreativityShorts, {
+    state: FormState.Default,
+  })
+
+  return (
+    <div
+      className="flex h-[calc(100%-12px)] w-full max-w-screen-sm flex-col overflow-hidden rounded-[0px_0px_20px_20px] bg-white px-7 pb-9 pt-5 md:my-auto md:h-full md:max-h-[720px] md:rounded-[20px] md:py-5 lg:max-h-[640px] lg:max-w-[480px] lg:rounded-xl lg:px-[52px] lg:pb-8"
+      onClick={(event) => event.stopPropagation()}
+    >
+      <p className="shrink-0 text-center text-lg font-bold leading-normal text-black">
+        短影音投稿
+      </p>
+      {response.state === FormState.Success ? (
+        <div className="flex shrink grow flex-col items-center justify-center">
+          <div className="flex flex-col items-center">
+            <p className="text-base font-medium leading-normal text-[#000928]">
+              送出成功！
+            </p>
+            <p className="mb-10 mt-1 text-xs font-normal leading-[150%] text-[#7F8493]">
+              影片審核完成後，將會寄送通知
+            </p>
+            <NextButton clickFn={closeHandler}>關閉</NextButton>
+          </div>
+        </div>
+      ) : (
+        <>
+          <form
+            className="mt-9 flex shrink grow flex-col items-center overflow-y-auto lg:mt-0"
+            method="POST"
+            encType="multipart/form-data"
+            action={formAction}
+          >
+            {isDesktop ? <></> : <MobileFormBody onClose={closeHandler} />}
+          </form>
+        </>
+      )}
+    </div>
+  )
+}
--- shared-components/shorts-upload/upload-modal.tsx
+++ shared-components/shorts-upload/upload-modal.tsx
@@ -2,33 +2,17 @@
 
 import { useDispatch } from 'react-redux'
 import { shortsUploadActions } from '@/redux/shorts-upload/slice'
-import MobileFormBody from './mobile-form-body'
-import { createCreativityShorts } from '@/app/actions-general'
-import { useWindowSize } from 'usehooks-ts'
-import { getTailwindConfig } from '@/utils/tailwind'
-import { useEffect, useMemo } from 'react'
-import { useFormState } from 'react-dom'
-import { FormState } from '@/types/shorts'
-import NextButton from './next-button'
+import { useEffect } from 'react'
 import { useAppSelector } from '@/redux/hooks'
 import { selectIsModalOpened } from '@/redux/shorts-upload/selector'
 import { usePathname, useSearchParams } from 'next/navigation'
+import ModalBody from './modal-body'
 
 export default function UploadModal() {
   const dispatch = useDispatch()
   const isModalOpened = useAppSelector(selectIsModalOpened)
   const pathname = usePathname
   const searchParams = useSearchParams()
-  const config = getTailwindConfig()
-  const desktopLowerBound = Number(config.theme.screens.lg.split('px')[0])
-  const { width } = useWindowSize()
-  const isDesktop = useMemo(
-    () => width >= desktopLowerBound,
-    [desktopLowerBound, width]
-  )
-  const [response, formAction] = useFormState(createCreativityShorts, {
-    state: FormState.Default,
-  })
 
   const closeHandler = () => {
     dispatch(shortsUploadActions.resetAllState())
@@ -47,36 +31,7 @@ export default function UploadModal() {
       className="fixed inset-x-0 bottom-[var(--shorts-header-height)] top-0 z-upload-modal flex flex-col items-center bg-[#7F8493]/80 md:bottom-0"
       onClick={closeHandler}
     >
-      <div
-        className="flex h-[calc(100%-12px)] w-full max-w-screen-sm flex-col overflow-hidden rounded-[0px_0px_20px_20px] bg-white px-7 pb-9 pt-5 md:my-auto md:h-full md:max-h-[720px] md:rounded-[20px] md:py-5 lg:max-h-[640px] lg:max-w-[480px] lg:rounded-xl lg:px-[52px] lg:pb-8"
-        onClick={(event) => event.stopPropagation()}
-      >
-        <p className="shrink-0 text-center text-lg font-bold leading-normal text-black">
-          短影音投稿
-        </p>
-        {response.state === FormState.Success ? (
-          <div className="flex shrink grow flex-col items-center justify-center">
-            <div className="flex flex-col items-center">
-              <p className="text-base font-medium leading-normal text-[#000928]">
-                送出成功！
-              </p>
-              <p className="mb-10 mt-1 text-xs font-normal leading-[150%] text-[#7F8493]">
-                影片審核完成後，將會寄送通知
-              </p>
-              <NextButton clickFn={closeHandler}>關閉</NextButton>
-            </div>
-          </div>
-        ) : (
-          <form
-            className="mt-9 flex shrink grow flex-col items-center overflow-scroll"
-            method="POST"
-            encType="multipart/form-data"
-            action={formAction}
-          >
-            {isDesktop ? <></> : <MobileFormBody onClose={closeHandler} />}
-          </form>
-        )}
-      </div>
+      <ModalBody key={String(isModalOpened)} closeHandler={closeHandler} />
     </div>
   )
 }
