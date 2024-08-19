e72f2f016512eda2f555be3f98f96467750f3cdb
Tsuki Akiba <erase2004@gmail.com>
2024-08-19 15:27:20 +0800

    refactor: move listener functions

--- redux/listener-middleware.ts
+++ redux/listener-middleware.ts
@@ -1,5 +1,10 @@
 import { createListenerMiddleware, addListener } from '@reduxjs/toolkit'
 import type { AppState, AppDispatch } from './store'
+import {
+  selectAutoImage,
+  selectManualImage,
+  selectShorts,
+} from './shorts-upload/selector'
 
 export const listenerMiddleware = createListenerMiddleware()
 
@@ -10,3 +15,52 @@ export const startStoreListening = listenerMiddleware.startListening.withTypes<
 >()
 
 export const addStoreListener = addListener.withTypes<AppState, AppDispatch>()
+
+/** revoke blob URL during related state changing to prevent memory leaks */
+startStoreListening({
+  predicate: (_, currentState, originalState) => {
+    return (
+      currentState.shortsUpload.shortsFileBlobURL !==
+      originalState.shortsUpload.shortsFileBlobURL
+    )
+  },
+  effect: (_, listenApi) => {
+    const shorts = selectShorts(listenApi.getOriginalState())
+    if (shorts.blobURL) {
+      console.log('revoke shorts blobURL //')
+      URL.revokeObjectURL(shorts.blobURL)
+    }
+  },
+})
+
+startStoreListening({
+  predicate: (_, currentState, originalState) => {
+    return (
+      currentState.shortsUpload.autoImageBlobURL !==
+      originalState.shortsUpload.autoImageBlobURL
+    )
+  },
+  effect: (_, listenApi) => {
+    const autoImage = selectAutoImage(listenApi.getOriginalState())
+    if (autoImage.blobURL) {
+      console.log('revoke autoImage blobURL //')
+      URL.revokeObjectURL(autoImage.blobURL)
+    }
+  },
+})
+
+startStoreListening({
+  predicate: (_, currentState, originalState) => {
+    return (
+      currentState.shortsUpload.manualImageBlobURL !==
+      originalState.shortsUpload.manualImageBlobURL
+    )
+  },
+  effect: (_, listenApi) => {
+    const manualImage = selectManualImage(listenApi.getOriginalState())
+    if (manualImage.blobURL) {
+      console.log('revoke manualImage blobURL //')
+      URL.revokeObjectURL(manualImage.blobURL)
+    }
+  },
+})
--- redux/store.ts
+++ redux/store.ts
@@ -1,57 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import shortsUploadReducer from './shorts-upload/slice'
-import { listenerMiddleware, startStoreListening } from './listener-middleware'
-import {
-  selectAutoImage,
-  selectManualImage,
-  selectShorts,
-} from './shorts-upload/selector'
-
-/** revoke blob URL during related state changing to prevent memory leaks */
-startStoreListening({
-  predicate: (_, currentState, originalState) => {
-    return (
-      currentState.shortsUpload.shortsFileBlobURL !==
-      originalState.shortsUpload.shortsFileBlobURL
-    )
-  },
-  effect: (_, listenApi) => {
-    const shorts = selectShorts(listenApi.getOriginalState())
-    if (shorts.blobURL) {
-      URL.revokeObjectURL(shorts.blobURL)
-    }
-  },
-})
-
-startStoreListening({
-  predicate: (_, currentState, originalState) => {
-    return (
-      currentState.shortsUpload.autoImageBlobURL !==
-      originalState.shortsUpload.autoImageBlobURL
-    )
-  },
-  effect: (_, listenApi) => {
-    const autoImage = selectAutoImage(listenApi.getOriginalState())
-    if (autoImage.blobURL) {
-      URL.revokeObjectURL(autoImage.blobURL)
-    }
-  },
-})
-
-startStoreListening({
-  predicate: (_, currentState, originalState) => {
-    return (
-      currentState.shortsUpload.manualImageBlobURL !==
-      originalState.shortsUpload.manualImageBlobURL
-    )
-  },
-  effect: (_, listenApi) => {
-    const manualImage = selectManualImage(listenApi.getOriginalState())
-    if (manualImage.blobURL) {
-      URL.revokeObjectURL(manualImage.blobURL)
-    }
-  },
-})
+import { listenerMiddleware } from './listener-middleware'
 
 export const makeStore = () => {
   return configureStore({
