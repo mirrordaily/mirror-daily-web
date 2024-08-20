7a25016e75af34b62babed46f5a790fa61591927
nihclil <oxx921@gmail.com>
2024-08-20 18:18:28 +0800

    refactor: adjust html tag and css

--- shared-components/ui-popular-news-card.tsx
+++ shared-components/ui-popular-news-card.tsx
@@ -11,30 +11,28 @@ export default function UiPopularNewsCard({
   heroImage,
 }: PopularNews): ReactElement {
   return (
-    <Link
-      href={link}
-      target="_blank"
-      rel="noopener noreferrer"
-      className="flex flex-col md:gap-y-2 lg:gap-y-3"
-    >
-      <figure className="relative overflow-hidden rounded md:h-[154px] lg:h-[133px]">
-        <CustomImage
-          images={heroImage?.resized}
-          imagesWebP={heroImage?.resizedWebp}
-          alt={postName}
-        />
-        <div
-          className={`absolute bottom-2 left-2 rounded-lg px-1 py-0 text-xs font-bold leading-4 tracking-[0.5px] text-[#ffffff]`}
-          style={{
-            backgroundColor: categoryColor || '#1C7CED',
-          }}
-        >
-          {categoryName}
+    <Link href={link} target="_blank" rel="noopener noreferrer">
+      <figure className="flex max-w-[280px] flex-col gap-y-2 lg:w-[240px] lg:gap-y-3">
+        <div className="relative max-h-[154px] overflow-hidden rounded lg:h-[133px]">
+          <CustomImage
+            images={heroImage.resized}
+            imagesWebP={heroImage.resizedWebp}
+            alt={postName}
+          />
+          <span
+            className={`absolute bottom-2 left-2 rounded-lg px-1 py-0 text-xs font-bold leading-4 tracking-[0.5px] text-[#ffffff]`}
+            style={{
+              backgroundColor: categoryColor,
+            }}
+          >
+            {categoryName}
+          </span>
         </div>
+
+        <figcaption className="line-clamp-2 text-lg font-normal text-[#000928] lg:line-clamp-3">
+          {postName}
+        </figcaption>
       </figure>
-      <figcaption className="text-lg font-normal text-[#000928] md:line-clamp-2 lg:line-clamp-3">
-        {postName}
-      </figcaption>
     </Link>
   )
 }
