006ccd21ee46e31ab64927cd43b30419075bba33
nihclil <oxx921@gmail.com>
2024-08-22 03:19:47 +0800

    feat: add types

---
+++ types/story-page.ts
@@ -0,0 +1,29 @@
+import type { HeroImage } from '@/types/common'
+import type { ApiData } from '@/shared-components/api-data-renderer/renderer'
+
+export type Post = {
+  title: string
+  subTitle: string
+  heroCaption: string
+  createdTime: string
+  postMainImage: HeroImage
+  sectionName: string
+  sectionColor: string
+  writers: string[]
+  photographers: string[]
+  apiData: ApiData
+  apiDataBrief: ApiData
+  tags: string[]
+}
+
+export type ItemInHeroSection = Omit<Post, 'apiData' | 'apiDataBrief'>
+
+//TODO: 整合相同 type
+export type RelatedPost = {
+  title: string
+  createdTime: string
+  link: string
+  sectionColor: string
+  sectionName: string
+  postMainImage: HeroImage
+}
