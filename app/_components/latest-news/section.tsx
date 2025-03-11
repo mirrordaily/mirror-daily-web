06d2a4eeb110cadbe1784b2cd6b3804572a9a4dc
nihclil <oxx921@gmail.com>
2025-03-12 03:43:39 +0800

    styles: adjust latest-news section

--- app/_components/latest-news/section.tsx
+++ app/_components/latest-news/section.tsx
@@ -8,10 +8,10 @@ type Props = {
 export default async function LatestNewsSection({ initialList }: Props) {
   return (
     <section className="section-in-homepage mt-9">
-      <p className="mb-[34px] text-base font-bold leading-normal text-[#119CC7] md:mb-[26px] md:text-lg">
+      <p className="mb-9 flex justify-center text-lg font-bold leading-normal text-[#896FCC] md:mb-[26px] md:justify-start lg:mb-[29px] lg:text-xl">
         最新新聞
       </p>
-      <div className="flex w-full flex-col items-center justify-center gap-y-4 pb-8 md:flex-row md:flex-wrap md:gap-x-10 md:gap-y-7 md:pb-[27px] lg:gap-x-8 lg:pb-12 [&>*:last-child]:flex [&>*:last-child]:w-full [&>*:last-child]:justify-center">
+      <div className="flex w-full flex-col items-center justify-center gap-y-[18px] pb-8 md:flex-row md:flex-wrap md:gap-x-10 md:gap-y-7 md:pb-[27px] lg:gap-9 lg:pb-[52px] [&>*:last-child]:flex [&>*:last-child]:w-full [&>*:last-child]:justify-center">
         <PostList initialList={initialList} fetchLatestPost={fetchLatestPost} />
       </div>
     </section>
