c3d27e0fa6e95640386a421b585ac0c8a566490a
YiHsuan <v61265@gmail.com>
2025-03-17 14:35:09 +0800

    feat: add comscore

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import '@/shared-styles/global.css'
 import type { Metadata } from 'next'
 import { Noto_Sans_TC } from 'next/font/google'
 import { GoogleTagManager } from '@next/third-parties/google'
+import Script from 'next/script'
 
 const notoSans = Noto_Sans_TC({
   preload: true,
@@ -35,6 +36,21 @@ export default function RootLayout({
   return (
     <html lang="zh-Hant" className={notoSans.className}>
       <GoogleTagManager gtmId={GTM_ID} />
+      <Script
+        id="comscore"
+        dangerouslySetInnerHTML={{
+          __html: `var _comscore = _comscore || [];
+                _comscore.push({ c1: "2", c2: "24318560" ,  options: { enableFirstPartyCookie: "true" } });
+                (function() {
+                  var s = document.createElement("script"), el = document.getElementsByTagName("script")[0]; s.async = true;
+                  s.src = "https://sb.scorecardresearch.com/cs/24318560/beacon.js";
+                  el.parentNode.insertBefore(s, el);
+                })();`,
+        }}
+      />
+      <noscript>
+        <img src="https://sb.scorecardresearch.com/p?c1=2&amp;c2=24318560&amp;cv=3.9.1&amp;cj=1" />
+      </noscript>
       <body className="app-layout">
         <StoreProvider>
           {children}
