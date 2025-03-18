00b75f4d345286f5ccffd4a8dd4a74e278f40db2
Tsuki Akiba <erase2004@gmail.com>
2025-03-18 13:49:27 +0800

    refactor: update tag page metadata

--- app/tag/[slug]/page.tsx
+++ app/tag/[slug]/page.tsx
@@ -2,12 +2,48 @@ import ArticlesSection from '../_components/articles-section'
 import PopularNewsSection from '@/shared-components/popular-news-section'
 import { fetchTagInformation, fetchTagPosts } from '../actions'
 import { notFound } from 'next/navigation'
+import type { Metadata } from 'next'
+import { SITE_NAME } from '@/constants/misc'
+import { getTagPageUrl } from '@/utils/site-urls'
+import { getDefaultMetadata } from '@/utils/common'
+
+type PageProps = {
+  params: { slug: string }
+}
+
+export async function generateMetadata({
+  params,
+}: PageProps): Promise<Metadata> {
+  const { slug } = params
+  const tagInfo = await fetchTagInformation(slug)
+
+  if (!tagInfo) {
+    notFound()
+  }
+
+  const defaultMetadata = getDefaultMetadata()
+
+  const title = `${tagInfo.name} - ${SITE_NAME}`
+
+  const metaData = Object.assign(
+    {},
+    {
+      ...defaultMetadata,
+      title,
+      openGraph: {
+        ...(defaultMetadata.openGraph ?? {}),
+        title,
+        url: getTagPageUrl(slug),
+      },
+    }
+  )
+
+  return metaData
+}
 
 export default async function Page({
   params,
-}: {
-  params: { slug: string }
-}): Promise<JSX.Element> {
+}: PageProps): Promise<JSX.Element> {
   const slug = params.slug
 
   const tagInfo = await fetchTagInformation(slug)
