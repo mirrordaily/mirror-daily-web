8efb24decff654039ef2a7922c350db90d0b9295
nihclil <oxx921@gmail.com>
2025-03-19 12:39:02 +0800

    styles: update primary color

--- shared-components/popular-news-section.tsx
+++ shared-components/popular-news-section.tsx
@@ -6,7 +6,7 @@ export default async function PopularNewsSection(): Promise<JSX.Element> {
 
   return (
     <section className="hidden md:flex md:w-[588px] md:flex-col md:items-center md:gap-y-[31px] lg:w-[240px] lg:gap-y-[19px]">
-      <p className="text-lg font-bold leading-normal text-[#119CC7]">
+      <p className="text-lg font-bold leading-normal text-[#674ab1]">
         熱門新聞
       </p>
       <div className="grid md:grid-cols-2 md:gap-7 lg:grid-cols-1 lg:gap-y-5">
