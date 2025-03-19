11e59014166c95e6721fd596a36e13327f9fb95e
YiHsuan <v61265@gmail.com>
2025-03-19 15:01:25 +0800

    fix(layout): hydrytion problem

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,24 +23,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="zh-Hant" className={notoSans.className}>
-      <GoogleTagManager gtmId={GTM_ID} />
-      <Script
-        id="comscore"
-        dangerouslySetInnerHTML={{
-          __html: `var _comscore = _comscore || [];
+      <head>
+        <GoogleTagManager gtmId={GTM_ID} />
+        <Script
+          id="comscore"
+          dangerouslySetInnerHTML={{
+            __html: `var _comscore = _comscore || [];
                 _comscore.push({ c1: "2", c2: "24318560" ,  options: { enableFirstPartyCookie: "true" } });
                 (function() {
                   var s = document.createElement("script"), el = document.getElementsByTagName("script")[0]; s.async = true;
                   s.src = "https://sb.scorecardresearch.com/cs/24318560/beacon.js";
                   el.parentNode.insertBefore(s, el);
                 })();`,
-        }}
-      />
-      <noscript
-        dangerouslySetInnerHTML={{
-          __html: `<img src="https://sb.scorecardresearch.com/p?c1=2&amp;c2=24318560&amp;cv=3.9.1&amp;cj=1">`,
-        }}
-      />
+          }}
+        />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<img src="https://sb.scorecardresearch.com/p?c1=2&amp;c2=24318560&amp;cv=3.9.1&amp;cj=1">`,
+          }}
+        />
+      </head>
       <body className="app-layout">
         <StoreProvider>
           {children}
