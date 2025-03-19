6f40ffe19492022bc0de31201f0d6f0ebd96dcef
nihclil <oxx921@gmail.com>
2025-03-19 12:42:30 +0800

    styles: update primary color

--- shared-components/error/popular-news-section.tsx
+++ shared-components/error/popular-news-section.tsx
@@ -8,7 +8,7 @@ export default function PopularNewsSection({
 }) {
   return (
     <section className="flex max-w-[240px] flex-col items-center gap-y-[19px] md:w-[588px] md:max-w-none md:gap-y-8">
-      <p className="text-lg font-bold leading-normal text-[##9a82da]">
+      <p className="text-lg font-bold leading-normal text-[#9a82da]">
         熱門新聞
       </p>
       <div className="grid grid-cols-1 gap-y-5 md:grid-cols-2 md:gap-7">
