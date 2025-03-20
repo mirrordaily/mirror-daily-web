1ad8d0f2fccdc21bcedd9faa08eb6fcafd91294d
Tsuki Akiba <erase2004@gmail.com>
2025-03-20 23:38:20 +0800

    feat: set cache-control headers

--- next.config.mjs
+++ next.config.mjs
@@ -6,34 +6,34 @@ const { SITE_BASE_PATH, IS_PREVIEW_MODE } = await tsImport.load(
 
 /** @typedef {import('next/dist/lib/load-custom-routes').Header['headers'][number]} Header  */
 
-// const SECOND = 1
-// const MINUTE = SECOND * 60
-// const HOUR = MINUTE * 60
-// const DAY = HOUR * 24
+const SECOND = 1
+const MINUTE = SECOND * 60
+const HOUR = MINUTE * 60
+const DAY = HOUR * 24
 
 /** @type {Header} */
-// const cacheControl5Min = {
-//   key: 'cache-control',
-//   value: `public, s-maxage=${MINUTE * 5}, stale-while-revalidate=60`,
-// }
+const cacheControl5Min = {
+  key: 'cache-control',
+  value: `public, s-maxage=${MINUTE * 5}, stale-while-revalidate=60`,
+}
 
-// /** @type {Header} */
+/** @type {Header} */
 // const cacheControl10Min = {
 //   key: 'cache-control',
 //   value: `public, s-maxage=${MINUTE * 10}, stale-while-revalidate=60`,
 // }
 
-// /** @type {Header} */
-// const cacheControl30Min = {
-//   key: 'cache-control',
-//   value: `public, s-maxage=${MINUTE * 30}, stale-while-revalidate=60`,
-// }
+/** @type {Header} */
+const cacheControl30Min = {
+  key: 'cache-control',
+  value: `public, s-maxage=${MINUTE * 30}, stale-while-revalidate=60`,
+}
 
 /** @type {Header} */
-// const cacheControl7Day = {
-//   key: 'cache-control',
-//   value: `public, s-maxage=${DAY * 7}, stale-while-revalidate=60`,
-// }
+const cacheControl7Day = {
+  key: 'cache-control',
+  value: `public, s-maxage=${DAY * 7}, stale-while-revalidate=60`,
+}
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -52,21 +52,49 @@ const nextConfig = {
       return []
     }
 
+    /** @see https://paper.dropbox.com/doc/CDN-cache---CicA78_qhXGqWFqc4NC99FkAAg-9oJDty7PW69K9YuC40aic */
     return [
-      // {
-      //   source: '/:path*',
-      //   headers: [
-      //     {
-      //       key: 'ETag',
-      //       value: `"${process.env.GIT_HASH}"`,
-      //     },
-      //     cacheControl5Min,
-      //   ],
-      // },
-      // {
-      //   source: '/story/:path*',
-      //   headers: [cacheControl7Day],
-      // },
+      // default, 5 minutes
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'ETag',
+            value: `"${process.env.GIT_HASH}"`,
+          },
+          cacheControl5Min,
+        ],
+      },
+      // 30 minutes
+      {
+        source: '/author/:path*',
+        headers: [cacheControl30Min],
+      },
+      {
+        source: '/tag/:path*',
+        headers: [cacheControl30Min],
+      },
+      {
+        source: '/section/:path*',
+        headers: [cacheControl30Min],
+      },
+      {
+        source: '/category/:path*',
+        headers: [cacheControl30Min],
+      },
+      {
+        source: '/topic',
+        headers: [cacheControl30Min],
+      },
+      // 7 days
+      {
+        source: '/story/:path*',
+        headers: [cacheControl7Day],
+      },
+      {
+        source: '/external/:path*',
+        headers: [cacheControl7Day],
+      },
     ]
   },
   poweredByHeader: false,
