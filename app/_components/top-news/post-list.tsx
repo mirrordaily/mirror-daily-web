222b429ba17c95bb2f2f208a6979cf7a55495bc1
nihclil <oxx921@gmail.com>
2025-05-06 12:10:49 +0800

    styles: add class

--- app/_components/top-news/post-list.tsx
+++ app/_components/top-news/post-list.tsx
@@ -17,7 +17,7 @@ export default function PostList({ list }: Props) {
   return (
     <div className="mt-4 flex w-full flex-col gap-y-[34px] md:flex-row lg:mt-3">
       <HighlightItem {...highlight} />
-      <div className="flex flex-col gap-y-3 md:ml-6 md:gap-y-2 lg:ml-5 lg:mt-[3px] lg:border-l-[0.5px] lg:border-l-[#000928] lg:pl-[21px]">
+      <div className="flex w-full flex-col gap-y-3 md:ml-6 md:gap-y-2 lg:ml-5 lg:mt-[3px] lg:border-l-[0.5px] lg:border-l-[#000928] lg:pl-[21px]">
         {others.map((post) => (
           <ListItem key={post.postId} {...post} />
         ))}
