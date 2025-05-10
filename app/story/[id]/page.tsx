fa567a357709c321742d88f6de8a8fd5c036da0f
YiHsuan <v61265@gmail.com>
2025-05-11 00:25:33 +0800

    refactor(story): adjust metadata for dable

--- app/story/[id]/page.tsx
+++ app/story/[id]/page.tsx
@@ -44,16 +44,13 @@ export async function generateMetadata({
         url: postData.link,
         images: image,
         type: 'website',
+      },
+      other: {
         'product:availability': 'oos',
         'article:published_time': new Date(
           postData.publishedTime
         ).toISOString(),
         'article:section': postData.sectionName || 'UnCategorized',
-        'dable:author': postData.writers?.[0]
-          ? postData.writers[0].name
-          : 'Unknown Author',
-        'dable:item_id': postData.id,
-        'section:color': postData.sectionColor,
       },
     }
   )
