9c4cd5b753d761518ac87b1aba9cfab7e338f4d4
YiHsuan <v61265@gmail.com>
2025-05-12 19:50:44 +0800

    style(search): handle title

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,9 +1,6 @@
-import { ENV } from '@/constants/config'
 import SearchComponent from './_components/miso-search'
-import { notFound } from 'next/navigation'
 
 export default function SearchPage() {
-  if (ENV === 'prod') notFound()
   return (
     <div>
       <SearchComponent />
