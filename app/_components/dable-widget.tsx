2dfbecb533dba708559c5d68bd1eb6db4104b1a8
YiHsuan <v61265@gmail.com>
2025-05-22 20:20:04 +0800

    refactor: use useMemo for width detection

--- app/_components/dable-widget.tsx
+++ app/_components/dable-widget.tsx
@@ -7,8 +7,8 @@ import {
   DABLE_DEV_WIDGET_CONFIG,
 } from '@/constants/ad'
 import { ENV } from '@/constants/config'
-import { useEffect, useState } from 'react'
-import useWindowDimensions from '@/hooks/use-window-dimensions'
+import { useEffect, useState, useMemo } from 'react'
+import { useWindowSize } from 'usehooks-ts'
 
 declare global {
   interface Window {
@@ -23,24 +23,24 @@ type DableWidgetProps = {
 }
 
 export default function DableWidget({ type, className }: DableWidgetProps) {
-  const { width } = useWindowDimensions()
+  const { width = 0 } = useWindowSize()
   const [shouldRender, setShouldRender] = useState(false)
   const isProd = ENV === 'prod'
   const config = isProd
     ? DABLE_PROD_WIDGET_CONFIG[type]
     : DABLE_DEV_WIDGET_CONFIG[type]
 
+  const isMobile = useMemo(() => width <= 768, [width])
+
   useEffect(() => {
     if (config.device === 'both') setShouldRender(true)
     if (!width) setShouldRender(false)
-    const isMobile = (width ?? 0) <= 768
     if (isMobile) {
       setShouldRender(config.device === 'mobile')
     } else {
       setShouldRender(config.device === 'pc')
     }
-    console.log('shouldRender', shouldRender)
-  }, [width])
+  }, [isMobile])
 
   return (
     shouldRender && (
