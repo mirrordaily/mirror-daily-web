1f6e80a6871668521e9b671ad4299b802aea8695
YiHsuan <v61265@gmail.com>
2025-05-22 23:28:03 +0800

    refactor(popin): adjust order of popin in story and external

--- app/story/_components/article-section.tsx
+++ app/story/_components/article-section.tsx
@@ -44,8 +44,8 @@ export default async function ArticleSection({
           {relatedPosts.length > 0 && (
             <RelatedNewsSection posts={relatedPosts} />
           )}
-          <div id="_popIn_recommend" className="hidden md:block"></div>
-          <div id="_popIn_recommend_word"></div>
+          <div id="_popIn_recommend_word" className="mt-6"></div>
+          <div id="_popIn_recommend" className="mt-6 hidden md:block"></div>
         </div>
       </div>
 
