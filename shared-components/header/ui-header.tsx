977d4ee2bd9e1d614b2ac12007372a300284cfde
nihclil <oxx921@gmail.com>
2025-06-10 13:33:17 +0800

    styles: update text styles

--- shared-components/header/ui-header.tsx
+++ shared-components/header/ui-header.tsx
@@ -73,15 +73,11 @@ export default function UiHeader({
           <div className="flex w-full justify-end">
             <div className="ml-auto mr-4 mt-[23px] flex shrink-0 md:mt-10 md:gap-x-[5px] lg:mr-0 lg:mt-8 lg:gap-x-[7px]">
               <a
-                className="flex h-[26px] w-24 items-center justify-center gap-x-[10px] rounded-[29px] border-2 border-white md:w-[124px]"
+                className="flex h-[26px] w-24 items-center justify-center gap-x-[10px] rounded-[29px] border-2 border-white text-sm leading-normal text-white md:w-[124px]"
                 href="/search"
               >
-                <span className="text-sm leading-normal text-white md:hidden">
-                  AI 搜尋
-                </span>
-                <span className="hidden text-sm leading-normal text-white md:block">
-                  AI 智慧搜尋
-                </span>
+                <span className="md:hidden">AI 搜尋</span>
+                <span className="hidden md:block">AI 智慧搜尋</span>
                 <span className="relative inline-block size-5">
                   <NextImage src={IconSearch} fill={true} alt="搜尋" />
                 </span>
