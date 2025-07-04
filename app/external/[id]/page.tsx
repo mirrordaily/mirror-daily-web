577a4e60ff4b111807791a873a48d1050c0ae478
nihclil <oxx921@gmail.com>
2025-07-04 17:55:12 +0800

    feat: put ad below story brief

--- app/external/[id]/page.tsx
+++ app/external/[id]/page.tsx
@@ -96,7 +96,7 @@ export default async function Page({ params }: PageProps) {
       </div>
       <hr className="hidden w-[680px] border border-[#000000] md:mb-9 md:block lg:mb-12 lg:mt-4 lg:w-[1128px]" />
       <section className="mb-[72px] mt-5 flex flex-col items-center md:mb-[76px] md:mt-9 lg:mb-[92px] lg:mt-[6px] lg:flex-row lg:items-start lg:justify-center lg:gap-x-[104px]">
-        <div>
+        <div className="max-w-screen-sm md:max-w-[600px] lg:max-w-screen-md">
           <ArticleIntro {...intro} />
           <Article brief={brief} content={content} />
 
