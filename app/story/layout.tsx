37ec39a8cb9ad066f7a9608fe66ddff9ab6c9adf
YiHsuan <v61265@gmail.com>
2025-07-10 15:26:34 +0800

    refactor(scripts): move dable script to route-specific layouts

--- app/story/layout.tsx
+++ app/story/layout.tsx
@@ -1,5 +1,6 @@
 import Header from '@/shared-components/header'
 import Footer from '@/shared-components/footer'
+import Script from 'next/script'
 
 export default function Layout({
   children,
@@ -9,6 +10,24 @@ export default function Layout({
   return (
     <>
       <Header />
+      {/* <!-- Begin Dable Script / For inquiries, visit http://dable.io --> */}
+      <Script
+        type="text/javascript"
+        id="dable-script"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(d,a,b,l,e,r) {
+              if(d[b] && d[b].q)return;d[b]=d[b]||function(){(d[b].q=d[b].q||[]).push(arguments)};e=a.createElement(l);
+              e.async=1;e.charset='utf-8';e.src='//static.dable.io/dist/plugin.min.js';
+              r=a.getElementsByTagName(l)[0];r.parentNode.insertBefore(e,r);
+              })(window,document,'dable','script');
+            dable('setService', 'mirrordaily.news');
+            dable('sendLogOnce');
+              `,
+        }}
+      />
+
+      {/* <!-- End Dable Script / For inquiries, visit http://dable.io --> */}
       {/* main content */}
       <div className="flex w-full max-w-screen-lg shrink-0 grow flex-col">
         {children}
