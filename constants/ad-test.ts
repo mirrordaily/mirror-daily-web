b2f7076710b5ca6dfc6a2548388ad9febe7194be
nihclil <oxx921@gmail.com>
2025-07-10 12:40:24 +0800

    feat: add test ad slots

---
+++ constants/ad-test.ts
@@ -0,0 +1,99 @@
+export const adSlots = {
+  // 測試版本
+  mirrordaily_home_PC_970x250_1: {
+    slotId: '/7682122/mirrordaily_970x250_1',
+    size: [970, 250],
+    adDivId: '1745289656550-0',
+    collapseEmptyDivs: true,
+  },
+  mirrordaily_list_970x250: {
+    slotId: '/7682122/mirrordaily_970x250_2',
+    size: [970, 250],
+    adDivId: '1745289682647-0',
+    collapseEmptyDivs: true,
+  },
+  mirrordaily_home_PC_728x90_1: {
+    slotId: '/7682122/mirrordaily_728x90',
+    size: [728, 90],
+    adDivId: '1745289708049-0',
+    collapseEmptyDivs: true,
+  },
+  mirrordaily_article_300x600_1: {
+    slotId: '/7682122/mirrordaily_300x600_1',
+    size: [300, 600],
+    adDivId: '1745289732426-0',
+    collapseEmptyDivs: true,
+  },
+  mirrordaily_article_PC_300x600_R2: {
+    slotId: '/7682122/mirrordaily_300x600_2',
+    size: [300, 600],
+    adDivId: '1745289753549-0',
+    collapseEmptyDivs: true,
+  },
+  mirrordaily_article_PC_640x390_AT1: {
+    slotId: '/7682122/mirrordaily_640x390',
+    size: [640, 390],
+    adDivId: '1745289773435-0',
+    collapseEmptyDivs: true,
+  },
+  mirrordaily_home_MW_336x280_HD: {
+    slotId: '/7682122/mirrordaily_336x280_1',
+    size: [336, 280],
+    adDivId: '1745289793308-0',
+    collapseEmptyDivs: true,
+  },
+  mirrordaily_list_MW_336x280_HD: {
+    slotId: '/7682122/mirrordaily_336x280_2',
+    size: [336, 280],
+    adDivId: '1745289814690-0',
+    collapseEmptyDivs: true,
+  },
+  mirrordaily_list_MW_336x280_FT: {
+    slotId: '/7682122/mirrordaily_336x280_3',
+    size: [336, 280],
+    adDivId: '1745289832640-0',
+    collapseEmptyDivs: true,
+  },
+  mirrordaily_article_MW_336x280_HD: {
+    slotId: '/7682122/mirrordaily_336x280_4',
+    size: [336, 280],
+    adDivId: '1745289850989-0',
+    collapseEmptyDivs: true,
+  },
+  mirrordaily_article_MW_336x280_AT1: {
+    slotId: '/7682122/mirrordaily_336x280_5',
+    size: [336, 280],
+    adDivId: '1745290244325-0',
+    collapseEmptyDivs: true,
+  },
+  mirrordaily_article_MW_336x280_AT2: {
+    slotId: '/7682122/mirrordaily_336x280_6',
+    size: [336, 280],
+    adDivId: '1745290294761-0',
+    collapseEmptyDivs: true,
+  },
+  mirrordaily_article_MW_336x280_AT3: {
+    slotId: '/7682122/mirrordaily_336x280_7',
+    size: [336, 280],
+    adDivId: '1745290316325-0',
+    collapseEmptyDivs: true,
+  },
+  mirrordaily_article_MW_336x280_E1: {
+    slotId: '/7682122/mirrordaily_336x280_8',
+    size: [336, 280],
+    adDivId: '1745290339369-0',
+    collapseEmptyDivs: true,
+  },
+  mirrordaily_list_MW_320x100_FIX: {
+    slotId: '/7682122/mirrordaily_320x100_1',
+    size: [320, 100],
+    adDivId: '1745290361351-0',
+    collapseEmptyDivs: true,
+  },
+  mirrordaily_article_MW_320x100_ST: {
+    slotId: '/7682122/mirrordaily_320x100_2',
+    size: [320, 100],
+    adDivId: '1745290385982-0',
+    collapseEmptyDivs: true,
+  },
+} as const
