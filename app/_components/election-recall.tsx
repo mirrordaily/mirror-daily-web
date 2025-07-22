fcacf0db7510263caada058c2ba50cef916b8067
nihclil <oxx921@gmail.com>
2025-07-22 12:34:44 +0800

    styles: adjust recall section styles

--- app/_components/election-recall.tsx
+++ app/_components/election-recall.tsx
@@ -6,12 +6,12 @@ import {
 export default function ElectionRecall() {
   return (
     <section className="section-in-homepage my-5 flex flex-col items-center">
-      <p className="text-center text-xl font-bold leading-none text-[#6C00AA]">
+      <p className="mb-4 text-center text-xl font-bold leading-none text-[#6C00AA]">
         2025 鏡報立委罷免即時開票
       </p>
       <iframe
         src={URL_ELECTION_RECALL_IFRAME}
-        className="h-[230px] w-full overflow-hidden md:h-[220px]"
+        className="mb-4 h-[240px] w-full overflow-hidden md:h-[230px]"
       />
       <a
         className="cursor-pointer text-base font-bold leading-none text-[#6C00AA] underline"
