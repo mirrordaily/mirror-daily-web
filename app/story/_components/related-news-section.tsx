b673e1d47bac059a4095daba7768ca8349c92273
Wendy Hu <yunhu412@gmail.com>
2025-07-28 15:51:41 +0800

    style: adjust margin-top for '你可能會喜歡' 和 '其他人也在看'

--- app/story/_components/related-news-section.tsx
+++ app/story/_components/related-news-section.tsx
@@ -6,8 +6,8 @@ type Props = {
 export default function RelatedNewsSection({ posts }: Props) {
   if (!posts.length) return null
   return (
-    <section className="flex flex-col items-center gap-y-8 pl-[23px] pr-[22px] md:px-0 lg:items-start lg:gap-y-7">
-      <h3 className="mt-7 text-lg font-bold leading-normal text-[#674ab1]">
+    <section className="mt-7 flex flex-col items-center gap-y-8 pl-[23px] pr-[22px] md:px-0 lg:items-start lg:gap-y-7">
+      <h3 className="text-lg font-bold leading-normal text-[#674ab1]">
         延伸閱讀
       </h3>
       <div className="grid grid-cols-1 gap-y-6 md:grid-cols-2 md:gap-7 lg:grid-cols-3 lg:gap-y-12">
