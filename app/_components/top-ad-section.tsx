f0a3b050afe68d1a31bceae822144128703be7bf
Wendy Hu <yunhu412@gmail.com>
2025-08-01 17:11:42 +0800

    feat(homepage): update TopAdSection

--- app/_components/top-ad-section.tsx
+++ app/_components/top-ad-section.tsx
@@ -4,30 +4,11 @@ import useAdBlockDetector from '@/hooks/use-ad-block-detector'
 import { DesktopGptAd } from '@/shared-components/gpt-ad/desktop-gpt-ad'
 import { MobileGptAd } from '@/shared-components/gpt-ad/mobile-gpt-ad'
 
-type TopAdSectionProps = {
-  isStagingOrProd: boolean
-}
-
-export default function TopAdSection({ isStagingOrProd }: TopAdSectionProps) {
+export default function TopAdSection() {
   const { isAdBlockerActive } = useAdBlockDetector()
 
-  if (isAdBlockerActive) return null // Don't render ads if ad blocker is active
-  return isStagingOrProd ? (
-    <>
-      <div className="hidden min-h-[306px] lg:block">
-        <DesktopGptAd
-          slotKey="mirrordaily_home_PC_970x250_1"
-          customClasses="mt-5 mb-9"
-        />
-      </div>
-      <div className="block min-h-[286px] md:hidden">
-        <MobileGptAd
-          slotKey="mirrordaily_list_MW_336x280_HD"
-          customClasses="my-9 mx-auto"
-        />
-      </div>
-    </>
-  ) : (
+  if (isAdBlockerActive) return null
+  return (
     <>
       <div className="hidden min-h-[306px] lg:flex lg:items-center">
         <DesktopGptAd
