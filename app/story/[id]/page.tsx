a90d65f8ed9a0ad76b1240c54d9f2bd28411c7da
Wendy Hu <yunhu412@gmail.com>
2025-08-04 11:34:56 +0800

    feat: create shared ArticlePageTopAd component

--- app/story/[id]/page.tsx
+++ app/story/[id]/page.tsx
@@ -9,10 +9,9 @@ import { getDefaultMetadata } from '@/utils/common'
 import { Suspense } from 'react'
 import PageLogger from '@/shared-components/page-logger'
 import AdultWarning from '../_components/adult-warning'
-import { DesktopGptAd } from '@/shared-components/gpt-ad/desktop-gpt-ad'
-import { MobileGptAd } from '@/shared-components/gpt-ad/mobile-gpt-ad'
 import MisoPageView from '@/shared-components/miso-pageview'
 import { ENV } from '@/constants/config'
+import ArticlePageTopAd from '@/shared-components/top-ads/article-page-top-ad'
 
 type PageProps = { params: { id: string } }
 
@@ -89,19 +88,7 @@ export default async function Page({ params }: PageProps) {
         <PageLogger extra={extra} />
       </Suspense>
       <main className="flex flex-col items-center">
-        <div className="hidden min-h-[306px] lg:flex lg:items-center">
-          <DesktopGptAd
-            slotKey="mirrordaily_article_PC_970x250_top"
-            customClasses="mt-5 mb-9"
-          />
-        </div>
-
-        <div className="block min-h-[286px] md:hidden">
-          <MobileGptAd
-            slotKey="mirrordaily_article_MW_300x250_top"
-            customClasses="mb-9"
-          />
-        </div>
+        <ArticlePageTopAd />
         <hr className="hidden w-[680px] border border-[#000000] md:mb-9 md:block lg:mb-12 lg:mt-4 lg:w-[1128px]" />
         <MisoPageView productIds={`story_${id}`} />
         <ArticleSection {...postData} id={id} />
