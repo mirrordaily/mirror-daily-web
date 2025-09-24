ae2137e5cb6fb2e7350edc2c72eab146739451fd
Wendy Hu <yunhu412@gmail.com>
2025-09-24 14:58:54 +0800

    refactor(story): simplify section slicing logic

--- app/story/actions.ts
+++ app/story/actions.ts
@@ -69,9 +69,7 @@ function transformPost(rawData: GetPostByIdQuery['post']): Post | null {
     })) ?? []
 
   const slicedSections = Array.isArray(rawData.sections)
-    ? rawData.sections.length === 1
-      ? rawData.sections.slice(0, 1)
-      : rawData.sections.slice(0, 3)
+    ? rawData.sections.slice(0, 3)
     : []
 
   const sections = slicedSections.map((section) => ({
