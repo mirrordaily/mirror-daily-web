58273f56caae7851037d7fa050af72ff6e4012be
nihclil <oxx921@gmail.com>
2025-10-23 14:45:43 +0800

    Reapply "feat: add google video ad"
    This reverts commit c7febb1c2524ac46d69bd8096d7e619b7a32227e.

--- shared-components/api-data-renderer/block-renderer/video-block.tsx
+++ shared-components/api-data-renderer/block-renderer/video-block.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client'
+import { useEffect, useRef } from 'react'
 
 import type {
   ApiDataBlockBase,
@@ -46,17 +48,173 @@ type VideoProps = {
   }
 }
 
+declare global {
+  interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    google?: any
+  }
+}
+
 const Video = ({ video }: VideoProps) => {
+  const adContainerRef = useRef<HTMLDivElement>(null)
+  const videoContentRef = useRef<HTMLVideoElement>(null)
+  const isAdPlaying = useRef<boolean>(false)
+  const isContentFinished = useRef<boolean>(false)
+  const adsManagerRef = useRef<any>(null)
+  const adDisplayContainerRef = useRef<any>(null)
+  const adsLoaderRef = useRef<any>(null)
+  const adsInitialized = useRef(false)
+
+  useEffect(() => {
+    if (
+      typeof window === 'undefined' ||
+      !window.google ||
+      !adContainerRef.current ||
+      !videoContentRef.current
+    )
+      return
+
+    const ima = window.google.ima
+
+    adDisplayContainerRef.current = new ima.AdDisplayContainer(
+      adContainerRef.current,
+      videoContentRef.current
+    )
+
+    const adsLoader = new ima.AdsLoader(adDisplayContainerRef.current)
+    adsLoaderRef.current = adsLoader
+
+    function onAdError(adErrorEvent: any) {
+      console.log('❌ Ad Error:', adErrorEvent.getError())
+      if (adsManagerRef.current) {
+        adsManagerRef.current.destroy()
+        adsManagerRef.current = null
+      }
+      videoContentRef.current?.play()
+    }
+
+    function onContentPauseRequested() {
+      isAdPlaying.current = true
+      if (videoContentRef.current) {
+        videoContentRef.current.pause()
+        videoContentRef.current.controls = false
+      }
+    }
+
+    function onContentResumeRequested() {
+      isAdPlaying.current = false
+      if (videoContentRef.current) {
+        videoContentRef.current.controls = true
+        if (!isContentFinished.current) {
+          videoContentRef.current.play()
+        }
+      }
+    }
+
+    function onAdLoaded(adEvent: any) {
+      const ad = adEvent.getAd()
+      if (!ad.isLinear()) {
+        videoContentRef.current?.play()
+      }
+    }
+
+    function onAdsManagerLoaded(adsManagerLoadedEvent: any) {
+      const adsRenderingSettings = new ima.AdsRenderingSettings()
+      adsRenderingSettings.restoreCustomPlaybackStateOnAdBreakComplete = true
+
+      adsManagerRef.current = adsManagerLoadedEvent.getAdsManager(
+        videoContentRef.current,
+        adsRenderingSettings
+      )
+
+      adsManagerRef.current.addEventListener(
+        window.google.ima.AdErrorEvent.Type.AD_ERROR,
+        onAdError
+      )
+      adsManagerRef.current.addEventListener(
+        window.google.ima.AdEvent.Type.CONTENT_PAUSE_REQUESTED,
+        onContentPauseRequested
+      )
+      adsManagerRef.current.addEventListener(
+        window.google.ima.AdEvent.Type.CONTENT_RESUME_REQUESTED,
+        onContentResumeRequested
+      )
+      adsManagerRef.current.addEventListener(
+        window.google.ima.AdEvent.Type.LOADED,
+        onAdLoaded
+      )
+    }
+
+    adsLoaderRef.current.addEventListener(
+      window.google.ima.AdsManagerLoadedEvent.Type.ADS_MANAGER_LOADED,
+      onAdsManagerLoaded,
+      false
+    )
+    adsLoaderRef.current.addEventListener(
+      window.google.ima.AdErrorEvent.Type.AD_ERROR,
+      onAdError,
+      false
+    )
+
+    const contentEndedListener = function () {
+      if (isAdPlaying.current) return
+      isContentFinished.current = true
+      adsLoader.contentComplete()
+    }
+    videoContentRef.current.onended = contentEndedListener
+
+    const adsRequest = new window.google.ima.AdsRequest()
+
+    adsRequest.adTagUrl =
+      'https://pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/single_ad_samples&sz=640x480&cust_params=sample_ct%3Dlinear&ciu_szs=300x250%2C728x90&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator='
+
+    adsRequest.linearAdSlotWidth = adContainerRef.current.clientWidth
+    adsRequest.linearAdSlotHeight = adContainerRef.current.clientHeight
+
+    adsRequest.nonLinearAdSlotWidth = adContainerRef.current.clientWidth
+    adsRequest.nonLinearAdSlotHeight = 150
+
+    adsLoader.requestAds(adsRequest)
+  }, [])
+
+  const playAds = () => {
+    if (!adsInitialized.current && adDisplayContainerRef.current) {
+      adDisplayContainerRef.current.initialize()
+      try {
+        adsManagerRef.current?.init(
+          videoContentRef.current!.clientWidth,
+          videoContentRef.current!.clientHeight,
+          window.google.ima.ViewMode.NORMAL
+        )
+        adsManagerRef.current?.start()
+        adsInitialized.current = true
+      } catch (e) {
+        videoContentRef.current?.play()
+      }
+    }
+  }
+
   const heroImage = video.heroImage?.resized?.original
   return (
-    <div className="video-block">
-      <video
-        src={video.url}
-        loop
-        controls
-        poster={heroImage}
-        preload="metadata"
+    <div id="mainContainer" className="relative">
+      <div
+        id="adContainer"
+        ref={adContainerRef}
+        className="absolute left-0 top-0 size-full"
       />
+      <div className="video-block">
+        <video
+          src={video.url}
+          loop
+          controls
+          poster={heroImage}
+          preload="metadata"
+          ref={videoContentRef}
+          onPlay={() => {
+            playAds()
+          }}
+        />
+      </div>
     </div>
   )
 }
