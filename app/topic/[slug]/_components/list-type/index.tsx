7af076ddb695183fc7676a61cf12eb3d93c212a9
nihclil <oxx921@gmail.com>
2026-02-05 03:59:57 +0800

    feat: add jsonld on topic page

--- app/topic/[slug]/_components/list-type/index.tsx
+++ app/topic/[slug]/_components/list-type/index.tsx
@@ -2,18 +2,21 @@ import { notFound } from 'next/navigation'
 import { fetchListTypeTopicPostBySlug } from '../../../action'
 import List from './list'
 import { PAGE_SIZE } from '@/constants/topic'
+import { SITE_URL } from '@/constants/config'
+import { IMAGE_PATH } from '@/constants/default-path'
 
 type Props = {
   slug: string
 }
 
 export default async function ListTypeListing({ slug }: Props) {
-  const { postsData, postsCount } = await fetchListTypeTopicPostBySlug({
-    slug,
-    take: PAGE_SIZE,
-    page: 1,
-    withAmount: true,
-  })
+  const { postsData: initialPosts, postsCount } =
+    await fetchListTypeTopicPostBySlug({
+      slug,
+      take: PAGE_SIZE,
+      page: 1,
+      withAmount: true,
+    })
 
   if (postsCount === 0) notFound()
 
@@ -27,12 +30,45 @@ export default async function ListTypeListing({ slug }: Props) {
     return postsData
   }
 
+  const jsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'ItemList',
+    itemListElement: initialPosts.map((post, index) => {
+      let imageUrl: string | undefined
+      if (typeof post.postMainImage === 'string') {
+        imageUrl = post.postMainImage
+      } else {
+        imageUrl = post.postMainImage.resized?.original
+      }
+      return {
+        '@type': 'ListItem',
+        position: index + 1,
+        item: {
+          '@type': 'NewsArticle',
+          name: post.title,
+          image: imageUrl || `${SITE_URL}${IMAGE_PATH}`,
+          dateCreated: new Date(post.formattedDate).toISOString(),
+          description: post.brief,
+          url: `${SITE_URL}${post.link}`,
+        },
+      }
+    }),
+  }
+
   return (
-    <List
-      pageSize={PAGE_SIZE}
-      totalAmount={postsCount}
-      initialList={postsData}
-      fetchMoreItem={fetchMorePosts}
-    />
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(jsonLd).replace(/</g, '\\u003c'),
+        }}
+      />
+      <List
+        pageSize={PAGE_SIZE}
+        totalAmount={postsCount}
+        initialList={initialPosts}
+        fetchMoreItem={fetchMorePosts}
+      />
+    </>
   )
 }
