13c4d1d43ef08fb0987440ba1d39055561088e0a
nihclil <oxx921@gmail.com>
2026-03-03 05:01:31 +0800

    styles: adjust swiper component styles

--- app/_components/promote-topic/swiper-component.tsx
+++ app/_components/promote-topic/swiper-component.tsx
@@ -2,8 +2,9 @@
 
 import PromoteTopicItem from './item'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Autoplay } from 'swiper/modules'
+import { Autoplay, Pagination } from 'swiper/modules'
 import 'swiper/css'
+import 'swiper/css/pagination'
 import type { PromoteTopicData } from '@/types/homepage'
 import { useState } from 'react'
 
@@ -13,11 +14,12 @@ type Props = {
 
 export default function SwiperComponent({ list }: Props) {
   const [visible, setVisible] = useState(true)
+  const validList = list.filter((item) => item.topics)
 
-  if (!visible) return null
+  if (!visible || !validList.length) return null
 
   return (
-    <div className="fixed right-[20px] top-1/2 z-promote-topic">
+    <div className="fixed right-[20px] top-1/2 z-promote-topic w-[75px] lg:w-[120px]">
       <button
         onClick={() => setVisible(false)}
         className="absolute right-0 top-0 z-20 flex size-5 -translate-y-1/2 translate-x-1/2 items-center justify-center rounded-full bg-mirror-blue-200 p-[5px] text-black shadow-[0_2.47px_2.47px_0_#00000040]"
@@ -25,21 +27,27 @@ export default function SwiperComponent({ list }: Props) {
         X
       </button>
       <Swiper
-        modules={[Autoplay]}
+        modules={[Autoplay, Pagination]}
+        pagination={{
+          clickable: true,
+          el: '.promote-topic-pagination',
+          bulletClass: 'promote-topic-swiper-bullet',
+          bulletActiveClass: 'promote-topic-swiper-bullet-active',
+          horizontalClass: 'promote-topic-swiper-pagination-horizontal',
+        }}
         slidesPerView={1}
         autoplay={{
           delay: 5000,
         }}
-        loop={true}
+        loop={validList.length > 1}
         className="relative"
       >
-        {list
-          .filter((item) => item.topics)
-          .map((item) => (
-            <SwiperSlide key={item.id} className="relative">
-              <PromoteTopicItem post={item} />
-            </SwiperSlide>
-          ))}
+        {validList.map((item) => (
+          <SwiperSlide key={item.id} className="relative">
+            <PromoteTopicItem post={item} />
+          </SwiperSlide>
+        ))}
+        <div className="promote-topic-pagination" />
       </Swiper>
     </div>
   )
