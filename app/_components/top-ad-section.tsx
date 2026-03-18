234caee5caf7c76861033268f3a91585c2024bdf
nihclil <oxx921@gmail.com>
2026-03-18 10:21:56 +0800

    styles: adjust top-ad min size

--- app/_components/top-ad-section.tsx
+++ app/_components/top-ad-section.tsx
@@ -10,7 +10,7 @@ export default function TopAdSection() {
   if (isAdBlockerActive) return null
   return (
     <div id="gpt-top-leaderboard">
-      <div className="mb-4 mt-5 hidden min-h-[306px] lg:flex lg:items-center">
+      <div className="mb-4 mt-5 hidden min-h-[250px] lg:flex lg:items-center">
         <DesktopGptAd mode="normal" slotKey="mirrordaily_home_PC_970x250_top" />
       </div>
       <div className="block min-h-[286px] lg:hidden">
