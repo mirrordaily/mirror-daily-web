bd496dc30cc0f28c67f904eca4c647e229b6fdbf
nihclil <oxx921@gmail.com>
2026-03-18 10:08:41 +0800

    styles: adjust top-ad margin

--- shared-components/top-ads/article-page-top-ad.tsx
+++ shared-components/top-ads/article-page-top-ad.tsx
@@ -10,18 +10,16 @@ export default function ArticlePageTopAd() {
   if (isAdBlockerActive) return null
   return (
     <div id="gpt-top-leaderboard">
-      <div className="hidden min-h-[306px] lg:flex lg:items-center">
+      <div className="mb-4 mt-5 hidden min-h-[306px] lg:flex lg:items-center">
         <DesktopGptAd
           mode="normal"
           slotKey="mirrordaily_article_PC_970x250_top"
-          customClasses="mt-5 mb-9"
         />
       </div>
       <div className="block min-h-[286px] md:hidden">
         <NonDesktopGptAd
           mode="normal"
           slotKey="mirrordaily_article_MW_300x250_top"
-          customClasses="mb-9"
         />
       </div>
     </div>
