1b7ca77aafcbef4d483dd552fd9e54bdc7d45e19
Wendy Hu <yunhu412@gmail.com>
2026-07-30 19:22:32 +0800

    feat(header): implement sticky layout and separate non-sticky flash news

--- shared-components/header/ui-header.tsx
+++ shared-components/header/ui-header.tsx
@@ -73,81 +73,87 @@ export default function UiHeader({
   )
 
   return (
-    <header className="w-full">
-      <div className="flex h-[64px] items-center justify-center bg-mirror-blue-700 md:h-[68px] lg:h-[101px]">
-        <div className="flex flex-1 items-center justify-between pl-4 pr-[21px] md:max-w-[1000px] md:pl-5 lg:max-w-screen-lg lg:px-9">
-          <a
-            href="/"
-            className={`${headerGtmEvents.logo} relative h-12 w-[100px] md:h-[42px] md:w-[88px] lg:h-[77px] lg:w-[160px] lg:shrink-0`}
-          >
-            <NextImage
-              src={IconLogo}
-              fill
-              alt="Logo"
-              className="aspect-[150/42] md:aspect-auto"
-            />
-          </a>
-          <div className="ml-auto lg:ml-[initial]">
-            <div className="ml-auto mr-5 flex lg:mr-0">
-              <a
-                className={`${headerGtmEvents.search} flex h-[26px] w-24 items-center justify-center gap-x-[10px] rounded-[29px] border-2 border-white text-sm leading-normal text-white md:w-[124px] lg:w-full`}
-                href="/search"
-              >
-                <span className="md:hidden">AI 搜尋</span>
-                <span className="hidden md:block">AI 智慧搜尋</span>
-                <span className="relative inline-block size-5">
-                  <NextImage src={IconSearch} fill={true} alt="搜尋" />
-                </span>
-              </a>
-            </div>
-            <div className="mt-[13px] hidden items-center justify-between lg:flex">
-              <div className="flex items-center space-x-[18px] lg:mr-[18px]">
-                <ImagesAd />
-                {CONTACT_LINKS_WITHOUT_FIRST.map((contactLink) => {
-                  return (
-                    <a
-                      key={contactLink.href + contactLink.name}
-                      className={`${
-                        headerGtmEvents[contactLink?.gtmKey] || ''
-                      } header-submit-button`}
-                      href={contactLink.href}
-                    >
-                      {contactLink.headerSubmitButtonName}
-                    </a>
-                  )
-                })}
+    <>
+      <header className="sticky top-0 z-global-header w-full bg-white">
+        <div className="flex h-[64px] items-center justify-center bg-mirror-blue-700 md:h-[68px] lg:h-[101px]">
+          <div className="flex flex-1 items-center justify-between pl-4 pr-[21px] md:max-w-[1000px] md:pl-5 lg:max-w-screen-lg lg:px-9">
+            <a
+              href="/"
+              className={`${headerGtmEvents.logo} relative h-12 w-[100px] md:h-[42px] md:w-[88px] lg:h-[77px] lg:w-[160px] lg:shrink-0`}
+            >
+              <NextImage
+                src={IconLogo}
+                fill
+                alt="Logo"
+                className="aspect-[150/42] md:aspect-auto"
+              />
+            </a>
+            <div className="ml-auto lg:ml-[initial]">
+              <div className="ml-auto mr-5 flex lg:mr-0">
+                <a
+                  className={`${headerGtmEvents.search} flex h-[26px] w-24 items-center justify-center gap-x-[10px] rounded-[29px] border-2 border-white text-sm leading-normal text-white md:w-[124px] lg:w-full`}
+                  href="/search"
+                >
+                  <span className="md:hidden">AI 搜尋</span>
+                  <span className="hidden md:block">AI 智慧搜尋</span>
+                  <span className="relative inline-block size-5">
+                    <NextImage src={IconSearch} fill={true} alt="搜尋" />
+                  </span>
+                </a>
               </div>
-              <div className="flex lg:shrink-0 lg:grow-0 lg:gap-x-2">
-                {ExtendedSocialLinks.map(({ name, href, icon }) => {
-                  const width = iconSizes[name] || 24
-                  return (
-                    <a
-                      key={name}
-                      href={href}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className={`${headerGtmEvents[name]}`}
-                    >
-                      <NextImage
-                        src={icon}
-                        alt={name}
-                        width={width}
-                        height={24}
-                      />
-                    </a>
-                  )
-                })}
+              <div className="mt-[13px] hidden items-center justify-between lg:flex">
+                <div className="flex items-center space-x-[18px] lg:mr-[18px]">
+                  <ImagesAd />
+                  {CONTACT_LINKS_WITHOUT_FIRST.map((contactLink) => {
+                    return (
+                      <a
+                        key={contactLink.href + contactLink.name}
+                        className={`${
+                          headerGtmEvents[contactLink?.gtmKey] || ''
+                        } header-submit-button`}
+                        href={contactLink.href}
+                      >
+                        {contactLink.headerSubmitButtonName}
+                      </a>
+                    )
+                  })}
+                </div>
+                <div className="flex lg:shrink-0 lg:grow-0 lg:gap-x-2">
+                  {ExtendedSocialLinks.map(({ name, href, icon }) => {
+                    const width = iconSizes[name] || 24
+                    return (
+                      <a
+                        key={name}
+                        href={href}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className={`${headerGtmEvents[name]}`}
+                      >
+                        <NextImage
+                          src={icon}
+                          alt={name}
+                          width={width}
+                          height={24}
+                        />
+                      </a>
+                    )
+                  })}
+                </div>
               </div>
             </div>
+            <MobileToggleAndNav sections={sections} />
           </div>
-          <MobileToggleAndNav sections={sections} />
         </div>
-      </div>
-      <hr className="h-px w-full bg-[#ccced4] md:hidden" />
-      <div className="mx-auto pb-[3px] md:max-w-[1000px] md:pb-[9px] lg:max-w-screen-lg lg:pb-[17px]">
-        <div className="mt-2 w-full overflow-x-auto overflow-y-hidden pl-4 pr-0 md:pl-5 lg:mt-[15px] lg:overflow-visible lg:px-9">
-          <NavList sections={sections} topics={topics} />
+        <hr className="h-px w-full bg-[#ccced4] md:hidden" />
+
+        <div className="mx-auto md:max-w-[1000px] lg:max-w-screen-lg">
+          <div className="w-full overflow-x-auto overflow-y-hidden py-2 pl-4 pr-0 md:pl-5 lg:overflow-visible lg:px-9 lg:py-[15px]">
+            <NavList sections={sections} topics={topics} />
+          </div>
         </div>
+      </header>
+
+      <div className="mx-auto w-full pb-[3px] md:max-w-[1000px] md:pb-[9px] lg:max-w-screen-lg lg:pb-[17px]">
         <div className="mt-[14px] flex w-full grow items-start pl-4 text-lg md:mt-[7px] md:pl-5 md:text-base lg:mt-3 lg:px-9">
           <p className="mr-[18px] mt-1 shrink-0 font-bold leading-none text-[#FF5457] md:mr-[7px] lg:mr-3">
             快訊
@@ -155,6 +161,6 @@ export default function UiHeader({
           <FlashNewsList items={flashNews} />
         </div>
       </div>
-    </header>
+    </>
   )
 }
